21d878e0afcd6e98a5c9c4f4f771730d507946c6
VladanKostic <kostic.m.vladan@gmail.com>
2018-10-13 18:18:23 +0200

    Dodata strana sa grafikonm. Podaci na grafikonu nisu povezani sa bazom.

--- app/routes.py
+++ app/routes.py
@@ -139,3 +139,26 @@ def search_majstorservis_results( search ):
         table1 = ResultsMajstorServis(results)
         table1.border = True
         return render_template('majstorservisrezultat.html', table=table1)
+
+@app.route('/graf')
+def graf():
+    labels = [
+        'JAN', 'FEB', 'MAR', 'APR',
+        'MAY', 'JUN', 'JUL', 'AUG',
+        'SEP', 'OCT', 'NOV', 'DEC'
+    ]
+
+    values = [
+        967.67, 1190.89, 1079.75, 1349.19,
+        2328.91, 2504.28, 2873.83, 4764.87,
+        4349.29, 6458.30, 9907, 16297
+    ]
+
+    colors = [
+        "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+        "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+        "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
+    line_labels=labels
+    line_values=values
+    return render_template('graf.html', title='Troskovnik vozlila', max=17000, labels=line_labels, values=line_values)
